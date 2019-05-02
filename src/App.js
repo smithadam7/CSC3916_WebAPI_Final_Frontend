2e4388f38cfc0253c5e42fa5ab435f133c6f3993
smithadam7 <adamu777@live.com>
2019-05-01 18:27:41 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -18,31 +18,3 @@ function App() {
 }
 
 export default App;
-
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
-
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-      />
-    );
-  }
-}
-
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDygGwTT_ShpZTg2mTWJ5EF2LBsK2B7AZA'
-})(MapContainer);
